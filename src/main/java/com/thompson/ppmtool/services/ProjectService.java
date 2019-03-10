1ab9f20cf86fb93262cb5f2fd585666effd07082
Qthomp <qthompsn@gmail.com>
2019-03-10 15:11:41 -0500

    add delete project by id

--- src/main/java/com/thompson/ppmtool/services/ProjectService.java
+++ src/main/java/com/thompson/ppmtool/services/ProjectService.java
@@ -47,4 +47,13 @@ public class ProjectService {
     public Iterable<Project> findAllProjects(){
         return projectRepository.findAll();
     }
+
+    public void deleteProjectByIdentifier(String projectId){
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if(project == null){
+            throw new ProjectIdException("Can not delete project with ID: '"+ projectId.toUpperCase() + "'. Project does not exist");
+        }
+        projectRepository.delete(project);
+    }
 }
--- src/main/java/com/thompson/ppmtool/web/ProjectController.java
+++ src/main/java/com/thompson/ppmtool/web/ProjectController.java
@@ -62,5 +62,11 @@ public class ProjectController {
         return projectService.findAllProjects();
     }
 
+    @DeleteMapping("/{projectId}")
+    public ResponseEntity<?> deleteProject(@PathVariable String projectId){
+        projectService.deleteProjectByIdentifier(projectId);
+
+        return new ResponseEntity<String>("Project with ID: " + projectId.toUpperCase() + "' was deleted.",HttpStatus.OK);
+    }
 
 }
